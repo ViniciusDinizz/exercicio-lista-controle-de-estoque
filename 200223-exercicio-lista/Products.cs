db601eed0a1db563eeaec5b9dfe65f6672f3ac30
ViniciusDinizz <viniciusdiniz.ti/2gmail.com>
2023-03-21 07:24:06 -0300

    Add project files.

---
+++ 200223-exercicio-lista/Products.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _200223_exercicio_lista
+{
+    internal class Products
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public int Quant { get; set; }
+
+        public Products(string nome, int id, int quant)
+        {
+            Nome = nome;
+            ID = id;
+            Quant = quant;
+        }
+
+        public void EditNome (string nome)
+        {
+            this.Nome = nome.ToUpper();
+        }
+
+        public void EditID(int id)
+        {
+            this.ID = id;
+        }
+
+        public void EditQuant(int quant)
+        {
+            this.Quant = quant;
+        }
+        public override string ToString()
+        {
+            return $"\nDados do produto:\nNome: {Nome} | ID: {ID} | Total: {Quant}\n";
+        }
+    }
+}
---
+++ 200223-exercicio-lista/Program.cs
@@ -0,0 +1,151 @@
+﻿using System.ComponentModel.DataAnnotations;
+using _200223_exercicio_lista;
+
+namespace ExercicioLista
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<Products> estoque = new List<Products>();
+            int operation;
+
+            do
+            {
+                int item;
+                operation = menu();
+                switch (operation)
+                {
+                    case 1:
+                        InserirItem();
+                        PrintEstoque();
+                        Console.Write("PressEnter...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        PrintEstoque();
+                        Console.Write("PressEnter...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        item = ProcurarItem();
+                        Console.WriteLine(estoque[item].ToString());
+                        EditarItem(item);
+                        Console.Clear();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        item = ProcurarItem();
+                        Console.WriteLine(estoque[item].ToString());
+                        estoque.Remove(DeleteContact(item));
+                        break;
+                    case 5:
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            } while (operation != 5);
+
+
+            Products DeleteContact(int i)
+            {
+                return estoque[i];
+            }
+
+            int menu()
+            {
+                int op;
+                Console.WriteLine("Escolha a opção\n1- Inserir Item\n2- Mostrar estoque\n3- Editar item\n4- Retirar Item\n5- Sair");
+                return op = ReceberInteiro();
+            }
+
+            void InserirItem()
+            {
+                Console.Clear();
+                Console.Write("Inserir Itens no estoque:\nNome Item: ");
+                string nome1 = Console.ReadLine();
+                Console.Write("id Item: ");
+                int id1 = ReceberInteiro();
+                Console.Write("Quantidade: ");
+                int quant1 = ReceberInteiro();
+                estoque.Add(new Products(nome1.ToUpper(), id1, quant1));
+            }
+
+            int ReceberInteiro()
+            {
+                int op = 0;
+                bool result;
+                do
+                {
+                    try
+                    {
+                        op = int.Parse(Console.ReadLine());
+                        result = true;
+                    }
+                    catch (Exception ex) { result = false; }
+                } while (result == false);
+                return op;
+            }
+
+            void EditarItem (int item)
+            {
+                Console.WriteLine("Editar:\n[1]- Nome | [2]- ID | [3]- Quantidade");
+                int op = ReceberInteiro();
+                switch (op)
+                {
+                    case 1:
+                        Console.Write("Digite o nome: ");
+                        string nome = Console.ReadLine();
+                        estoque[item].EditNome(nome);
+                        break;
+                    case 2:
+                        Console.Write("Digite o ID: ");
+                        int id = ReceberInteiro();
+                        estoque[item].EditID(id);
+                        break;
+                    case 3:
+                        Console.Write("Digite a quantidade: ");
+                        int quant = ReceberInteiro();
+                        estoque[item].EditQuant(quant);
+                        break;
+                    default:
+                        Console.WriteLine("Opção Inválida.");
+                        break;
+                }
+            }
+
+            void PrintEstoque()
+            {
+                foreach (Products item in estoque)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+
+
+            int ProcurarItem()
+            {
+                PrintEstoque();
+                Console.Write("Digite o ID do item: ");
+                int id = ReceberInteiro();
+                Console.Clear();
+                for (int i = 0; i < estoque.Count; i++)
+                {
+                    if (estoque[i].ID == id)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+        }
+    }
+}
